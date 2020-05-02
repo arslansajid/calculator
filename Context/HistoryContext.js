c76185264a8231674841e2515daff8a114bb56f5
Arslan Sajid <arslansajid951@gmail.com>
2020-05-03 03:46:24 +0500

    removed folders

--- Context/HistoryContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const HistoryContext = React.createContext('');
-
-export const HistoryProvider = HistoryContext.Provider;
-export const HistoryConsumer = HistoryContext.Consumer;
-export default HistoryContext;
